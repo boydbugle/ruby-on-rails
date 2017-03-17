865ea3218fcee6c97b03f5706f8cd0dc979c815c
ndongA <boydbugle@gmai.com>
2017-03-17 13:37:02 +0300

    creates model

--- portfolio/app/models/project.rb
+++ portfolio/app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ApplicationRecord
+    belongs_to :skill
+    validates :name, :presence => true
+    validates :description, :presence => true
+    validates :link, :presence => true
 end
--- portfolio/app/models/skill.rb
+++ portfolio/app/models/skill.rb
@@ -1,2 +1,4 @@
 class Skill < ApplicationRecord
+    has_many :projects
+    validates :language, :presence => true
 end
