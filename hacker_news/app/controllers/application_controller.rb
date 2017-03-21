235ece3a555b0d423982821cb3850f5e855c4fba
ndongA <boydbugle@gmai.com>
2017-03-21 14:14:41 +0300

    creates authentication from scratch

--- hacker_news/app/controllers/application_controller.rb
+++ hacker_news/app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :require_login, only:[:edit, :update]
+
+  private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+  
+  def require_login
+     current_user=nil?
+      flash[:alert] = "You must be logged in to access this section"
+      redirect_to login_url 
+  end
 end
--- hacker_news/config/routes.rb
+++ hacker_news/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root :to => 'posts#index'
+get '/users/new', to: 'users#new', as: 'signup'
+get '/sessions/new', to: 'sessions#new', as: 'login'
+
+  resources :users
+  resources :sessions
+
+  root to: 'posts#index'
 
   resources :posts do
     resources :comments, :except =>[:show]
