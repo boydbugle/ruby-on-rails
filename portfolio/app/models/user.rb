1f975094ba0cc7709ceb4fae5c26f4dd907a3e86
ndongA <boydbugle@gmai.com>
2017-03-27 09:41:33 +0300

    creates basic authentication for user

---
+++ portfolio/app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+
+    attr_accessor :password
+
+    validates_confirmation_of :password
+    # validates_uniqueness_of :email
+
+    before_save :encrypt_password
+
+    def self.authenticate(email, password)
+        user = User.where(email: email).first
+
+        if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+
+            user
+
+        else
+
+            nil
+
+        end
+    end
+
+    def encrypt_password
+
+        self.password_salt = BCrypt::Engine.generate_salt
+
+        self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+
+    end
+    
+    
+end
--- portfolio/config/routes.rb
+++ portfolio/config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+
   root :to => 'skills#index'
 
+  get "/log-in", to: "sessions#new"
+
+  post "/log-in", to: "sessions#create"
+
+  get "/log-out", to: "sessions#destroy", as: :log_out
+
   get 'skills/admin'
+
+  resources :users
 
    resources :skills do
      resources :projects, :except => [:index]
