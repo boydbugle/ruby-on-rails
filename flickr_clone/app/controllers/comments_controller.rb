1cedb09c229e5b6edb7ba2e13f5b4f89d298bc26
ndongA <boydbugle@gmai.com>
2017-03-27 11:30:18 +0300

    creates relationship for members and comments

--- flickr_clone/app/controllers/comments_controller.rb
+++ flickr_clone/app/controllers/comments_controller.rb
@@ -7,7 +7,8 @@ class CommentsController < ApplicationController
   def create
     @photo = Photo.find(params[:photo_id])
     @comment = @photo.comments.new(comment_params)
-    if @comment..user_id(current_user).save
+    @comment.user = current_user
+    if @comment.save
       redirect_to photo_path(@photo)
     else 
       render :new
@@ -15,6 +16,25 @@ class CommentsController < ApplicationController
   end
 
   def edit
+     @photo = Photo.find(params[:photo_id])
+     @comment = Comment.find(params[:id])
+  end
+
+  def update
+     @photo = Photo.find(params[:photo_id])
+     @comment = Comment.find(params[:id])
+     if @comment.update(comment_params)
+       redirect_to photo_path(@photo)
+     else
+       render :edit
+      end
+  end
+
+  def destroy
+    @photo = Photo.find(params[:photo_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to photo_path(@photo)
   end
 
   private
