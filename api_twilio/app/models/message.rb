69864b135c154ed5d4c2bcad13f8d641c281b693
ndongA <boydbugle@gmai.com>
2017-04-06 16:44:38 +0300

    creates test for twilio message

--- api_twilio/app/models/message.rb
+++ api_twilio/app/models/message.rb
@@ -4,6 +4,7 @@ class Message < ActiveRecord::Base
   private
 
     def send_message
+      begin
         response = RestClient::Request.new(
             :method => :post,
             :url => "https://api.twilio.com/2010-04-01/Accounts/#{ENV['TWILIO_ACCOUNT_SID']}/Messages.json",
@@ -13,5 +14,10 @@ class Message < ActiveRecord::Base
                         :To => to,
                         :From => from }
         ).execute
+        rescue RestClient::BadRequest => error
+          message = JSON.parse(error.response)['message']
+          errors.add(:base, message)
+          throw(:abort)
+      end
     end
   end
--- api_twilio/spec/models/message_spec.rb
+++ api_twilio/spec/models/message_spec.rb
@@ -1,13 +1,13 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-#   describe Message, :vcr => true do
-#     it "doesn't save the message if twilio gives an error" do
-#       message = Message.new(:body => 'hi', :to => '1111111', :from => '5039463641')
-#       message.save.should be false
-#     end
-#      it 'adds an error if the to number is invalid' do
-#       message = Message.new(:body => 'hi', :to => '1111111', :from => '5039463641')
-#       message.save
-#       message.errors.messages[:base].should eq ["The 'To' number 1111111 is not a valid phone number."]
-#     end
-#   end
+  describe Message, :vcr => true do
+    it "doesn't save the message if twilio gives an error" do
+      message = Message.new(:body => 'hi', :to => '1111111', :from => '+1 334-269-8209 ')
+      message.save.should be false
+    end
+     it 'adds an error if the to number is invalid' do
+      message = Message.new(:body => 'hi', :to => '1111111', :from => '+1 334-269-8209 ')
+      message.save
+      message.errors.messages[:base].should eq ["The 'To' number 1111111 is not a valid phone number."]
+    end
+  end
