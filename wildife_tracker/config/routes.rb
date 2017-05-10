a5280fef8fe2d64bc77272fc95c4e335b7bd0ef0
ndongA <boydbugle@gmai.com>
2017-05-10 15:37:56 +0300

    create map display sighting#map

--- wildife_tracker/config/routes.rb
+++ wildife_tracker/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-root :to => 'sightings#index'
+root :to => 'sightings#map'
+  
+get 'map', to: 'sightings#map', as: 'sightings/map'
 
   resources :sightings do
     resources :animals, :except => [:show, :index]
