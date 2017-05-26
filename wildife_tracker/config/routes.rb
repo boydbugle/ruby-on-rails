52f1106d8c477a28f292712036d987deaec24a49
ndongA <boydbugle@gmai.com>
2017-05-26 12:40:28 +0300

    changes root and sightings#index

--- wildife_tracker/config/routes.rb
+++ wildife_tracker/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-root :to => 'sightings#map'
+root :to => 'sightings#index'
   
-get 'map', to: 'sightings#map', as: 'sightings/map'
+# get 'map', to: 'sightings#map', as: 'sightings/map'
 
   resources :sightings do
     resources :animals, :except => [:show, :index]
